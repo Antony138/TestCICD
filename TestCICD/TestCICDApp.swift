3f242b401711f4e08a7cf321b3c8e3937ce5bcae
Antony <antony1380@gmail.com>
2022-09-18 13:37:31 +0900

    Create test project.

---
+++ TestCICD/TestCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCICDApp.swift
+//  TestCICD
+//
+//  Created by Antony on 2022/09/18.
+//
+
+import SwiftUI
+
+@main
+struct TestCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
