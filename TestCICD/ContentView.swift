a54380c0608aee9d7f285759f96f37612427b890
Antony <antony1380@gmail.com>
2022-11-04 13:29:02 +0900

    Small fix.

--- TestCICD/ContentView.swift
+++ TestCICD/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, World-1")    
+            Text("Hello, Unit test")    
         }
         .padding()
     }
