ac551690aba5b7a407dfee80acefe755a3c11a35
Antony.Wong <antony1380@gmail.com>
2022-12-03 14:41:39 +0900

    Access build setting secrt data via info file.

--- TestCICD/ContentView.swift
+++ TestCICD/ContentView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    let myTestSecrt: String
+    let myLowercaseSecrt: String
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Unit Test in M1 (steup LC_ALL=en_US.UTF-8)")    
+            Text("Hello, \(myTestSecrt)")
+            Text("Hello, \(myLowercaseSecrt)")
         }
         .padding()
     }
@@ -21,6 +25,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(myTestSecrt: "MY_TEST_SECRT", myLowercaseSecrt: "my_lowercase_secrt")
     }
 }
--- TestCICD/TestCICDApp.swift
+++ TestCICD/TestCICDApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct TestCICDApp: App {
+    let myTestSecrt = Bundle.main.object(forInfoDictionaryKey: "MY_TEST_SECRT") as? String ?? "CAN NOT GET MY TEST SECRT"
+    let myLowercaseSecrt = Bundle.main.object(forInfoDictionaryKey: "my_lowercase_secrt") as? String ?? "CAN NOT GET LOWERCASE SECRT"
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(myTestSecrt: myTestSecrt, myLowercaseSecrt: myLowercaseSecrt)
         }
     }
 }
