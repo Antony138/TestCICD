275790a87a5b748821f48b836cbda6792dcc1820
Antony.Wong <antony1380@gmail.com>
2022-12-12 16:53:45 +0900

    Dev/secrets configration file (#26)
    * Update packge.
    
    * Smll fix of GitHub action
    
    * Add secrets configuration file.
    
    * Access build setting secrt data via info file.
    
    * Add debug build
    
    * Add new scheme for debug.
    
    * Add Alpha & check enviroment
    
    * Use   SWIFT_ACTIVE_COMPILATION_CONDITIONS
    
    * Add if: ! github.event.pull_request.draft
    
    * Small fix
    
    * Update Decrypt Config.xcconfig for project
    
    * Update Config.xcconfig
    
    * Trigger
    
    * Add ready_for_review
    
    * Test run name.
    
    * Update deploy_message
    
    * Small fix
    
    * Update deploygate_feedback_message
    
    * Update message.
    
    * Update message
    
    * get the branch name
    
    * get the branch name.
    
    * Test message.
    
    * Test git branch
    
    * test
    
    * Add debug message.
    
    * Update
    
    * Add branch name
    
    * Build new version after merge PR.

--- TestCICD/ContentView.swift
+++ TestCICD/ContentView.swift
@@ -8,12 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    let myTestSecrt: String
+    let myLowercaseSecrt: String
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Unit Test in M1 (steup LC_ALL=en_US.UTF-8)")    
+            Text("Hello, \(myTestSecrt)")
+            Text("Hello, \(myLowercaseSecrt)")
+            Button("Check current Environment") {
+                print("Current configuration: \(BuildConfiguration.shared.environment)")
+
+#if DEBUG
+                print("Current is DEBUG")
+#else
+                print("Current is not DEBUG")
+#endif
+
+                // 注意，这个是利用了SWIFT_ACTIVE_COMPILATION_CONDITIONS
+#if ALPHA
+                print("Current is ALPHA")
+#else
+                print("Current is not ALPHA")
+#endif
+                
+            }
         }
         .padding()
     }
@@ -21,6 +42,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(myTestSecrt: "MY_TEST_SECRT", myLowercaseSecrt: "my_lowercase_secrt")
     }
 }
--- fastlane/README.md
+++ fastlane/README.md
@@ -21,6 +21,22 @@ For _fastlane_ installation instructions, see [Installing _fastlane_](https://do
 
 
 
+### match_debug_development_adhoc
+
+```sh
+[bundle exec] fastlane match_debug_development_adhoc
+```
+
+
+
+### match_alpha_development_adhoc
+
+```sh
+[bundle exec] fastlane match_alpha_development_adhoc
+```
+
+
+
 ----
 
 
